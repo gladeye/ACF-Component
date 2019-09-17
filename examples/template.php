ca91fc63dca9087f93bf0f510944ba0f03b8c85a
Daniel Bonham <daniel@gladeye.co.nz>
2019-09-17 14:07:42 +1200

    Add an example of a template

---
+++ examples/template.php
@@ -0,0 +1,20 @@
+<?php
+use Kethatril\ACFComponent\Template;
+
+(new Template('home', 'Home', [
+    [
+        [
+            'param' => 'page_template',
+            'operator' => '==',
+            'value' => 'home'
+        ]
+    ]
+], [], true, 'home'))
+    ->addComponent('home-hero', 'home_hero', 'Hero')
+    ->addComponent('home-introduction', 'home_introduction', 'Introduction')
+    ->addComponent('featured-stores', 'featured_stores', 'Featured Stores', 'inline', ['extraClasses' => 'blue'])
+    ->addComponent('product-slider', 'product_slider', 'Product Slider')
+    ->addComponent('content-tiles', 'content_tiles', 'Content Tiles')
+    ->addComponent('merchant-cta', 'merchant_cta', 'Merchant Call to Action')
+    ->setDebug()
+    ->apply();
