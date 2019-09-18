63f6b0ca45c8e877f4b4b13218c9f08807ca41d4
Daniel Bonham <daniel@gladeye.co.nz>
2019-09-18 14:53:28 +1200

    Finish repeater field example

--- examples/tile-repeater.php
+++ examples/tile-repeater.php
@@ -6,7 +6,7 @@ use Kethatril\ACFComponent\Template;
 Template::registerComponent(
     (new StandardComponent('tile_repeater', 'Tile Repeater'))
         ->addField('text', 'title', 'Title')
-        ->addRepeaterField('')
+        ->addRepeaterField('tiles', 'Tiles', 'tile', 1, 3)
 );
 
 
